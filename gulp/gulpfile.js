2b2ecff9febb3d78e602fba286e7b92c017adf3e
Peter Kos <pkos91@icloud.com>
2014-10-14 22:24:14 -0400

    Added 'example' styling to gulp

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -15,16 +15,16 @@ gulp.task('browser-sync', function() {
     });
 });
 
-// gulp.task('page-styles', function() {
-//   return gulp.src('../example/example.css')
-//     //Parse
-//     .pipe(sass({ style: 'expanded' }))
-//     .pipe(autoprefixer('last 4 versions'))
-//     .pipe(gulp.dest('example'))
-//     //Reload
-//     .pipe(reload({stream:true}));
-//     // .pipe(notify({ message: 'Styles task complete' }));
-// });
+gulp.task('page-styles', function() {
+  return gulp.src('../example/example.css')
+    //Parse
+    .pipe(sass({ style: 'expanded' }))
+    .pipe(autoprefixer('last 4 versions'))
+    .pipe(gulp.dest('example'))
+    //Reload
+    .pipe(reload({stream:true}));
+    // .pipe(notify({ message: 'Styles task complete' }));
+});
 
 gulp.task('sweet-alert-styles', function() {
     return gulp.src('../lib/sweet-alert.scss')
@@ -40,7 +40,7 @@ gulp.task('sweet-alert-styles', function() {
 
 // Default task to be run with gulp
 gulp.task('default', ['sweet-alert-styles', 'browser-sync'], function () {
-    // gulp.watch("../example/*.scss", ['page-styles'])
+    gulp.watch("../example/*.scss", ['page-styles'])
     gulp.watch("../lib/sweet-alert.scss", ['sweet-alert-styles']);
 
 });
