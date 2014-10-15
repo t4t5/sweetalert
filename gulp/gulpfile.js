3692a6292de3333dac4d2460029c1aa42350a5d1
Peter Kos <pkos91@icloud.com>
2014-10-14 22:12:04 -0400

    Added gulp support

---
+++ gulp/gulpfile.js
@@ -0,0 +1,46 @@
+var gulp         = require('gulp');
+var rename       = require('gulp-rename');
+var browserSync  = require('browser-sync');
+var reload       = browserSync.reload;
+var notify       = require('gulp-notify');
+var sass         = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: "../",
+            index: "example/index.html"
+        }
+    });
+});
+
+// gulp.task('page-styles', function() {
+//   return gulp.src('../example/example.css')
+//     //Parse
+//     .pipe(sass({ style: 'expanded' }))
+//     .pipe(autoprefixer('last 4 versions'))
+//     .pipe(gulp.dest('example'))
+//     //Reload
+//     .pipe(reload({stream:true}));
+//     // .pipe(notify({ message: 'Styles task complete' }));
+// });
+
+gulp.task('sweet-alert-styles', function() {
+    return gulp.src('../lib/sweet-alert.scss')
+    //Parse
+    .pipe(sass({ style: 'expanded' }))
+    .pipe(autoprefixer('last 4 versions'))
+    .pipe(gulp.dest('../lib'))
+    //Reload
+    .pipe(reload({stream:true}))
+    .pipe(notify({ message: 'Sweet-alert task complete' }));
+});
+  
+
+// Default task to be run with gulp
+gulp.task('default', ['sweet-alert-styles', 'browser-sync'], function () {
+    // gulp.watch("../example/*.scss", ['page-styles'])
+    gulp.watch("../lib/sweet-alert.scss", ['sweet-alert-styles']);
+
+});
