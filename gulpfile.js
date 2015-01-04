51ab87641332b2ea84e5486c3f970afa52d71e83
Limon Monte <limon.monte@gmail.com>
2015-01-04 18:46:05 +0100

    Add functionality to disable CSS modal animation.

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,14 @@ var sass = require('gulp-sass');
 var rename = require('gulp-rename');
 
 gulp.task('compress', function() {
-  gulp.src('lib/sweetalert2.js')
+  return gulp.src('lib/sweetalert2.js')
     .pipe(uglify())
     .pipe(rename({extname: '.min.js'}))
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('sass', function() {
-  gulp.src('lib/sweetalert2.scss')
+  return gulp.src('lib/sweetalert2.scss')
     .pipe(sass())
     .pipe(gulp.dest('dist'));
 });
