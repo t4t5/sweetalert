38abe3d4559dd3893069d838ecd22599443eec3e
Stephan Bijzitter <StephanBijzitter@gmail.com>
2016-02-23 16:51:34 +0100

    Optimize the key listener

--- dev/modules/handle-key.js
+++ dev/modules/handle-key.js
@@ -1,17 +1,17 @@
 import { stopEventPropagation, fireClick } from './handle-dom';
 import { setFocusStyle } from './handle-swal-dom';
 
+var vk_tab = 9;
+var vk_esc = 27;
+var vk_enter = 13;
+var vk_space = 32;
+var key_codes = [vk_tab, vk_esc, vk_enter, vk_space];
 
 var handleKeyDown = function(event, params, modal) {
   var e = event || window.event;
   var keyCode = e.keyCode || e.which;
 
-  var $okButton     = modal.querySelector('button.confirm');
-  var $cancelButton = modal.querySelector('button.cancel');
-  var $modalButtons = modal.querySelectorAll('button[tabindex]');
-
-
-  if ([9, 13, 32, 27].indexOf(keyCode) === -1) {
+  if (key_codes.indexOf(keyCode) < 0) {
     // Don't do work on keys we don't care about.
     return;
   }
@@ -20,6 +20,10 @@ var handleKeyDown = function(event, params, modal) {
     stopEventPropagation(e);
     return;
   }
+
+  var $okButton     = modal.querySelector('button.confirm');
+  var $cancelButton = modal.querySelector('button.cancel');
+  var $modalButtons = modal.querySelectorAll('button[tabindex]');
 
   var $targetElement = e.target || e.srcElement;
 
@@ -31,8 +35,7 @@ var handleKeyDown = function(event, params, modal) {
     }
   }
 
-  if (keyCode === 9) {
-    // TAB
+  if (keyCode === vk_tab) {
     if (btnIndex === -1) {
       // No button focused. Jump to the confirm button.
       $targetElement = $okButton;
@@ -52,20 +55,20 @@ var handleKeyDown = function(event, params, modal) {
       setFocusStyle($targetElement, params.confirmButtonColor);
     }
   } else {
-    if (keyCode === 13) {
+    if (keyCode === vk_enter && params.allowReturnKey === true) {
       if ($targetElement.tagName === 'INPUT') {
         $targetElement = $okButton;
         $okButton.focus();
       }
 
-      if (btnIndex === -1 && params.allowReturnKey === true) {
+      if (btnIndex === -1) {
         // ENTER/SPACE clicked outside of a button.
         $targetElement = $okButton;
       } else {
         // Do nothing - let the browser handle it.
         $targetElement = undefined;
       }
-    } else if (keyCode === 27 && params.allowEscapeKey === true) {
+    } else if (keyCode === vk_esc && params.allowEscapeKey === true) {
       $targetElement = $cancelButton;
       fireClick($targetElement, e);
     } else {
