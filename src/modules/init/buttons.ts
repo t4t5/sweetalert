af86556f8911fe7a58cf8bc422624470ac104930
Lion Ralfs <lion.ralfs@hotmail.com>
2017-11-18 18:15:22 +0100

    Included possibility for multiple classes via an array

--- src/modules/init/buttons.ts
+++ src/modules/init/buttons.ts
@@ -8,9 +8,9 @@ import { ButtonList, ButtonOptions, CONFIRM_KEY } from '../options/buttons';
 import { footerMarkup, buttonMarkup } from '../markup';
 
 import { onAction } from '../actions';
-import { 
-  setActionValue, 
-  setActionOptionsFor, 
+import {
+  setActionValue,
+  setActionOptionsFor,
   ActionOptions,
 } from '../state';
 
@@ -33,7 +33,17 @@ const getButton = (namespace: string, {
   buttonEl.classList.add(btnNamespaceClass);
 
   if (className) {
-    buttonEl.classList.add(className);
+    if (typeof className === 'string') {
+      className.split(' ')
+        .filter(name => name.length > 0)
+        .forEach(name => {
+          buttonEl.classList.add(name);
+        });
+    } else if (Array.isArray(className)) {
+      className.forEach(name => {
+        buttonEl.classList.add(name);
+      });
+    }
   }
 
   if (dangerMode && namespace === CONFIRM_KEY) {
